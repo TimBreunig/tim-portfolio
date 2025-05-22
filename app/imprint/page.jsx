53f01d4397e692904416751142fc9bd7d81608d8
TimBreunig <breunig.tim@web.de>
2025-05-23 01:13:31 +0200

    Modified imprint and privacy policy

--- app/imprint/page.jsx
+++ app/imprint/page.jsx
@@ -2,15 +2,15 @@ const Imprint = () => {
   return (
 	<section className="h-full">
         <div className="mx-auto h-screen xl:pb-24 bg-radial-gradient text-primary-900">
-            <div className="container h-screen flex flex-col items-center justify-center">
-                <div className="mt-12 text-center text-lg text-primary-800">
+            <div className="container pt-20 xl:pt-0 xl:h-screen flex flex-col xl:flex-row items-center justify-between">
+                <div className="mt-12 w-full lg:w-2/3 text-left text-lg text-primary-800">
                     <h1 className="h1 text-primary-900">
                         Imprint
 				    </h1>
-                    <h2 className="h2 text-primary-900">
-                        Angaben gemäß § 5 TMG
-				    </h2>
-                    <p>
+                    <p className="font-bold text-xl mb-12">
+                        Information according to § 5 TMG
+				    </p>
+                    <p className="mb-9">
                         Tim Breunig<br />
                         Etivalstraße 52<br />
                         75173 Pforzheim<br />
@@ -19,9 +19,12 @@ const Imprint = () => {
                     <h2 className="h2 text-primary-900">
                         Contact
 				    </h2>
-                    <p>
+                    <p className="mb-9">
                         <a href="mailto:breunig.tim@web.de">breunig.tim@web.de</a><br />
                         <a href="tel:+49 162 5636484">+49 162 5636484</a>
+                    </p>
+                    <p>
+                        Source: eRecht24
                     </p>
                 </div>
 		    </div>
