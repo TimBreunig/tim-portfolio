ea81fe45c505447076d3f22a4e379120f75de5c2
TimBreunig <breunig.tim@web.de>
2026-01-09 21:34:02 +0100

    Added vercel speed insights.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import PageTransition from "@/components/PageTransition";
@@ -20,6 +21,7 @@ export default function RootLayout({ children }) {
             {children}
         </PageTransition>
         <Footer />
+        <SpeedInsights />
       </body>
     </html>
   );
