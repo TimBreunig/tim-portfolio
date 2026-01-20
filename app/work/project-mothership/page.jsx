5f4842bc62e8fb46f6ee427544f90ba1becdc7ad
TimBreunig <breunig.tim@web.de>
2026-01-20 13:58:08 +0100

    Fixed Project Mothership page bug.

--- app/work/project-mothership/page.jsx
+++ app/work/project-mothership/page.jsx
@@ -10,9 +10,9 @@ const ProjectMothership = () => {
             description="In this cooperative action-adventure game, players embark on an intergalactic journey where teamwork, exploration, and puzzle-solving are key to success. The game revolves around your mothership. Work together to navigate mysterious planets and solve intricate puzzles, all while enhancing the mothership to tackle increasingly challenging missions. The thrill of discovery and the wonder of the unknown await you!"
             images={[
                 "/images/project_mothership_teaser.png",
-                "/images/project_mothership_teaser.png",
+                "/images/project_mothership_teaser2.png",
                 "/images/project_mothership_teaser3.png",
-                "/images/project_mothership_teaser.png",
+                "/images/project_mothership_teaser4.png",
             ]}
         />
     )
